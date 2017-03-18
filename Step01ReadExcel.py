7f19cc850c4e2927f2132b9faff78324bd404ad8
jimmysyss <tszming.wong@gmail.com>
2017-03-18 17:29:03 +0800

    Initial Commit

--- Step01ReadExcel.py
+++ Step01ReadExcel.py
@@ -11,12 +11,6 @@ def init_db():
     engine = create_engine('sqlite:///./stock.db', convert_unicode=True)
     Base.metadata.create_all(engine)
 
-    # Sample Stock
-    # stock = Stock()
-    # stock.reuter = '0001.HK'
-    # stock.bloomberg = '1:HK'
-    # stock.isin = 'ABC1234567'
-
     wb = load_workbook(filename="data/isino.xlsx")
     ws = wb['ISIN']
 
@@ -25,6 +19,8 @@ def init_db():
 
     # From A20 onwards, A => Long Name / B => ISIN / C => Stock Code / D => Type
     row = 20
+    new_stock = 0
+    update_stock = 0
     while ws.cell(row=row, column=1).value is not None:
         # Check for Type
         type = ws.cell(row=row, column=4).value
@@ -32,15 +28,29 @@ def init_db():
             row += 1
             continue
 
-        stock = Stock()
-        stock.fullname = ws.cell(row=row, column=1).value
-        stock.isin = ws.cell(row=row, column=2).value
-        stockCode = ws.cell(row=row, column=3).value
-        stock.reuter = "{:04}.HK".format(stockCode)
-        stock.bloomberg = "{:d}:HK".format(stockCode)
+        stock_code = ws.cell(row=row, column=3).value
+        reuter_code = "{:04}.HK".format(stock_code)
+        bloomberg_code = "{:d}:HK".format(stock_code)
+        isin = ws.cell(row=row, column=2).value
+        fullname = ws.cell(row=row, column=1).value
 
-        session.add(stock)
+        existing_stock = session.query(Stock).filter_by(reuter=reuter_code).first()
+
+        if existing_stock is None :
+            # NEW
+            stock = Stock(reuter=reuter_code, bloomberg=bloomberg_code, isin=isin, fullname=fullname)
+            session.add(stock)
+            new_stock += 1
+        else:
+            # UPDATE
+            existing_stock.bloomberg = bloomberg_code
+            existing_stock.isin = isin
+            existing_stock.fullname = fullname
+            update_stock += 1
+
         row += 1
+
+    print('New {0} / Update {1}'.format(new_stock, update_stock))
 
     session.commit()
 
