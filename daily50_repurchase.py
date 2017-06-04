3967861012a35433e0eb2bde2aff5ab399024289
jimmysyss <tszming.wong@gmail.com>
2017-06-05 00:27:59 +0800

    Remove Bonodo

--- daily50_repurchase.py
+++ daily50_repurchase.py
@@ -1,44 +1,14 @@
-import xlrd
-import bonobo
-import dataset
+import petl
 
-#db = dataset.connect('sqlite:///./stock.db')
-#table = db['repurchases']
+def etl():
+    # Extract
+    input_data = petl.fromxls(filename='data/SRRPT20170317.xls', sheet='SBNReport')
 
-def generate_data():
-    xlsBook = xlrd.open_workbook(filename='data/SRRPT20170317.xls')
-    sheet = xlsBook.sheet_by_name('SBNReport')
-    row = 5
-    while sheet.cell(row, 1).value:
-        stock_code = sheet.cell(row, 1).value
-        stock_purchase = sheet.cell(row, 4).value
-        purchase_price = sheet.cell(row, 5).value
-        row += 1
-        yield dict(stock_code=stock_code, stock_purchase=stock_purchase, purchase_price=purchase_price)
+    # Transform
+    trimmed_head_data = petl.skip(input_data, 4)
 
+    print(trimmed_head_data)
 
-def my_transform(i: dict) -> dict:
-    new_dict = dict(
-        stock_code=i['stock_code'],
-        stock_purchase=int(i['stock_purchase'].replace(",", "")),
-        purchase_price=float(i['purchase_price'].split()[1]),
-    )
-    yield new_dict
-
-
-def my_load(s: dict):
-    # print(s)
-    db = dataset.connect('sqlite:///./stock.db')
-    print('HELLO')
-    table = db['repurchases']
-    table.insert(s)
-
-
-graph = bonobo.Graph(
-    generate_data,
-    my_transform,
-    my_load,
-)
 
 if __name__ == '__main__':
-    bonobo.run(graph)
+    etl()
